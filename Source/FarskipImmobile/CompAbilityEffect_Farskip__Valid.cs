36506955c9bf6de9901c9385bde8c759ecf2f585
MaxDorob <maxdor81@gmail.com>
2025-07-23 21:41:35 +0600

    Removed reflection (+performance)

--- Source/FarskipImmobile/CompAbilityEffect_Farskip__Valid.cs
+++ Source/FarskipImmobile/CompAbilityEffect_Farskip__Valid.cs
@@ -6,10 +6,9 @@ using Verse;
 namespace FarskipImmobile
 {
 	[HarmonyPatch(typeof(CompAbilityEffect_Farskip), "Valid")]
-	public class CompAbilityEffect_Farskip_Valid
+	public static class CompAbilityEffect_Farskip_Valid
 	{
-		[HarmonyPostfix]
-		private static void Postfix(CompAbilityEffect_Farskip __instance, GlobalTargetInfo target, bool throwMessages, ref bool __result)
+		public static void Postfix(CompAbilityEffect_Farskip __instance, GlobalTargetInfo target, bool throwMessages, ref bool __result)
 		{
 
 			Caravan caravan = __instance.parent.pawn.GetCaravan();
@@ -19,14 +18,7 @@ namespace FarskipImmobile
 			{
 				return;
 			}
-
-			var method_ShouldEnterMap = __instance.GetType().GetMethod("ShouldEnterMap", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
-			var method_ShouldJoinCaravan = __instance.GetType().GetMethod("ShouldJoinCaravan", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
-
-			bool var_ShouldEnterMap = (bool)method_ShouldEnterMap.Invoke(__instance, new object[] { target });
-			bool var_ShouldJoinCaravan = (bool)method_ShouldJoinCaravan.Invoke(__instance, new object[] { target });
-
-			if(var_ShouldEnterMap || var_ShouldJoinCaravan)
+			if(__instance.ShouldEnterMap(target) || __instance.ShouldJoinCaravan(target))
             {
 				__result = true;
             }
